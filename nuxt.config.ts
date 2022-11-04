3c9ecea32662c0258e65b72b6419d59b6bedca0c
Nick Notararigo <nicolas.notararigo@gmail.com>
2022-11-04 16:42:12 +0100

    feat(firebase): add firebase and standard maintenance page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,15 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-16',
-      viewport: 'width=500, initial-scale=1', 
+      viewport: 'width=500, initial-scale=1',
       title: 'My App',
       meta: [
         // <meta name="description" content="My amazing site">
         { name: 'description', content: 'My amazing site.' }
       ],
+      // link: [
+      //   { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      // ]
     }
   },
   vite: {
---
+++ pages/maintenance.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="maintenance">
+    <h1>Maintenance</h1>
+    <h2>Space-cube.xyz</h2>
+    <br />
+    <p>
+      Nous travaillons sur une toute nouvelle version du site, merci de votre
+      compréhension (:
+    </p>
+    <br />
+    <p>
+      Vous pouvez toujours nous rejoindre et discuter avec nous sur notre
+      <a href="https://invite.gg/spacecancer" target="_blank">discord</a>.
+    </p>
+    <br />
+    <p>&copy; 2022 - Tous droits réservés</p>
+  </div>
+</template>
+<script setup>
+useMeta({
+  title: "Space-Cube - Serveur minecraft - Site en Maintenance",
+});
+</script>
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+#__nuxt,
+.maintenance {
+  padding: 1rem;
+  font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+  height: 100%;
+  display: flex;
+  background-color: #13110c;
+  color: white;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+a {
+  color: #f8d270;
+  font-weight: bold;
+  text-decoration: none;
+}
+a:hover {
+  color: #d6b256;
+  font-weight: bold;
+  text-decoration: underline;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif;
+}
+</style>
