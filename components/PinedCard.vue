a5056800e8a92522f022200cdde4dd0276e0bcae
Nick <nicolas.notararigo@gmail.com>
2023-01-27 15:38:11 +0100

    feat: add peepslords page

--- components/PinedCard.vue
+++ components/PinedCard.vue
@@ -1,27 +1,38 @@
 <template>
-  <section :class="`pined-card ${invertInfos}`" :style="`background-image: url('${cardImg}')`">
+  <section :class="`pined-card invert-${invertInfos}`" :style="`background-image: url('${cardImg}')`">
     <div class="pined-card-infos">
-      <h3>{{ title }}</h3>
-      <p>{{ description }}</p>
-      <a :href="ctaPath">
+      <div class="d-flex row justify-content-between">
+        <div v-if="isIconHidden" class="pined-card-col-icon">
+          <i :class="`pi pi-${iconName}`"></i>
+        </div>
+        <div class="pined-card-col-infos">
+          <h3>{{ title }}</h3>
+          <p>{{ description }}</p>
+        </div>
+        <div v-if="isShowServerInfos" class="pined-card-col-server_infos d-flex flex-column">
+            <div class="d-flex row count-players">17/50</div>
+            <div class="d-flex row">players</div>
+        </div>
+      </div>
+      <NuxtLink :to="ctaPath">
         <button class="cta-pined-card">{{ ctaLabel}}</button>
-      </a>
+      </NuxtLink>
     </div>
   </section>
 </template>
 
 <script>
 export default {
-  name: "pined-card",
+  name: 'pined-card',
   props: {
     title: {
       type: String,
-      default: "title",
+      default: 'title',
       required: true
     },
     description: {
       type: String,
-      default: "description",
+      default: 'description',
       required: true
     },
     ctaPath: {
@@ -34,28 +45,60 @@ export default {
     },
     cardImg: {
       type: String,
-      default: "https://wallpapercave.com/wp/wp10584479.png",
+      default: 'https://wallpapercave.com/wp/wp10584479.png',
       required: true
     },
     invertInfos: {
       type: Boolean,
       default: false
+    },
+    iconName: {
+      type: String,
+      default: 'hourglass'
+    },
+    isIconHidden: {
+      type: Boolean,
+      default: false
+    },
+    isShowServerInfos: {
+      type: Boolean,
+      default: false
     }
   }
-}
+};
 </script>
 <style lang="scss">
 .pined-card {
   width: calc(50% - 30px);
-  background-size: 100%;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
   max-height: 19rem;
+  height: 19rem;
   min-height: 13rem;
   padding: 14px 24px;
   display: flex;
-  align-items: end;
+  align-items: flex-end;
   border-radius: 1rem;
-  .invertInfos {
+
+  &.invert-true {
     justify-content: end;
+  }
+
+  .count-players {
+    font-size: 1.2rem;
+    font-weight: 400;
+  }
+
+  i.pi {
+    background: linear-gradient(24.06deg, #50C9C3 2.61%, #B6DE96 118.09%);
+    // box-shadow: 0px 13px 17px rgba(140, 213, 105, 0.6);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    text-decoration: none;
+    font-size: 1.5rem;
+    margin-right: 1rem;
+    margin-top: 0.3rem;
   }
 
   .pined-card-infos {
@@ -63,11 +106,39 @@ export default {
     backdrop-filter: blur(6px);
     padding: 14px 24px;
     height: 50%;
+    width: 357px;
     border-radius: 1rem;
     border: 1px solid #60A5FA;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+    h3 {
+      margin: 0;
+    }
+
+    p {
+      margin: 6px 0;
+      width: 200px;
+      font-size: 12px;
+      line-height: 1rem;
+    }
+
+    a {
+      text-decoration: none !important;
+      background-image: none;
+      width: 100%;
+    }
 
     .cta-pined-card {
+      cursor: pointer;
       color: white;
+      border-radius: 12px !important;
+      border: 1px solid #60A5FA !important;
+      padding: 8px 24px;
+      height: 36px !important;
+      width: 100%;
     }
   }
 }
---
+++ pages/servers/peepslords.vue
@@ -0,0 +1,53 @@
+<template>
+  <main>
+     <div class="d-flex row justify-content-between">
+        <h1>👑 Serveur des PeepsLords</h1>
+        <div class="d-flex row">
+            <a class="h1" href="https://livemap.minestrator.com/s/e3a02394-181e-4c33-9079-8d0ee940a4c1/#overworld:0:0:0:1500:0:0:0:0:perspective" target="_blank">
+                <span class="h1 white">Map</span>
+            </a>&nbsp;
+            <a class="h1" href="https://www.curseforge.com/minecraft/modpacks/medieval-mc-fabric" target="_blank">
+                <span class="h1 white">Modpack</span>
+            </a>
+        </div>
+     </div>
+      <hr>
+     <section>
+        <h2>Ip du serveur :</h2>
+        <code id="ipServer">
+            46.105.38.71:25575
+        </code>
+        <h2>Version du jeu :</h2>
+        <code id="versionGame">
+            1.19.2
+        </code>
+        <h2>Version du modpack :</h2>
+        <code id="ipServer">
+            v14.5
+        </code>
+     </section>
+      <hr>
+     <section>
+        <h4>Comment installer le modpack ?</h4>
+            <ol class="ml-5">
+                <li>Télécharger CurseForge</li>
+                <li>Sur CurseForge, choisissez le jeu Minecraft</li>
+                <li>Dans la barre de recherche en haut, recherchez le modpack <a class="white" href="https://www.curseforge.com/minecraft/modpacks/medieval-mc-fabric" title="Accéder au modpack via CurseForge">Medieval</a> (version 14.5)</li>
+                <li>Le modpack est chargé, penser à importer vos ressources pack et vos shaders</li>
+                <li>Cliquer sur Play et attention à remettre vos paramétrages (contrôles, render distance, etc...)</li>
+            </ol>
+     </section>
+  </main>
+  </template>
+
+<script>
+export default {
+  name: 'PeepsLords',
+  layout: 'default',
+  head () {
+    return {
+      title: 'Space-Cube - 👑 Serveur des PeepsLords'
+    };
+  }
+};
+</script>
