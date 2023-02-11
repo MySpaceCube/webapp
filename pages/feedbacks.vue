81375ed255d3565c6023041c5e8bc792a3d29ba4
Nick <nicolas.notararigo@gmail.com>
2023-02-11 21:42:11 +0100

    feat: global add

--- pages/feedbacks.vue
+++ pages/feedbacks.vue
@@ -1,10 +1,51 @@
 <template>
-  <h1>feedbacks</h1>
+  <div>
+    <h1>Feedbacks</h1>
+    <section>
+      <h2>Feedback - Pin</h2>
+      <section class="pined-card-section">
+        <PinedCard
+          title="SpaceCube Hardcore"
+          description="Tu vas manger tes grands morts"
+          cta-label="play.space-cube.xyz"
+          cta-path="/servers/spacecube"
+          :is-icon-hidden="false"
+          is-show-server-infos
+          card-img="https://www.minecraft.net/content/dam/games/badger/key-art/MC_Badger-Gather-Your-Strength_1200x540.png.transform/minecraft-image-large/image.png"
+        />
+        <PinedCard
+          title="PeepsLords"
+          description="Serveur Moddes avec le modpack Medieval"
+          cta-label="peepslords.space-cube.xyz"
+          cta-path="/servers/peepslords"
+          card-img="https://www.minecraft.net/content/dam/games/minecraft/key-art/MC-Dungeons_Image-0_XGP-Dungeons_1200x432.jpg.transform/minecraft-image-large/image.jpg"
+          invert-infos
+          :is-icon-hidden="false"
+          is-show-server-infos
+        />
+      </section>
+    </section>
+    <section>
+      <h2>Other Feedback</h2>
+      <ul>
+        <li>
+          blabla
+        </li>
+      </ul>
+    </section>
+
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'FeedbacksPage'
+  name: 'FeedbacksPage',
+  layout: 'default',
+  head () {
+    return {
+      title: 'Space-Cube | Feedbacks'
+    };
+  }
 };
 </script>
 
--- pages/server.vue
+++ pages/server.vue
@@ -1,10 +1,46 @@
 <template>
-  <h1>Server</h1>
+    <div>
+    <h1>SpaceCube - Server</h1>
+    <h2>Our Server</h2>
+    <section class="pined-card-section">
+      <PinedCard
+        title="SpaceCube Hardcore"
+        description="Tu vas manger tes grands morts"
+        cta-label="play.space-cube.xyz"
+        cta-path="/servers/spacecube"
+        :is-icon-hidden="false"
+        is-show-server-infos
+        card-img="https://wallpaperaccess.com/full/7782767.jpg"
+      />
+      <PinedCard
+        title="PeepsLords"
+        description="Serveur Moddes avec le modpack Medieval"
+        cta-label="peepslords.space-cube.xyz"
+        cta-path="/servers/peepslords"
+        card-img="https://img2.goodfon.com/wallpaper/nbig/6/9c/minecraft-dom-sklad-trava.jpg"
+        invert-infos
+        :is-icon-hidden="false"
+        is-show-server-infos
+      />
+    </section>
+    <section>
+      <h2>Other servers</h2>
+      <ul>
+        <li>bla bla</li>
+      </ul>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'ServerPage'
+  name: 'ServerPage',
+  layout: 'default',
+  head () {
+    return {
+      title: 'Space-Cube | Serveur minecraft'
+    };
+  }
 };
 </script>
 
