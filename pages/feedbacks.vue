26050b25647ee20d43c27c37eb74f8ee4239d0af
Nick Notararigo <nicolas.notararigo@gmail.com>
2023-02-20 19:04:14 +0100

    feat: add fetch to feedbacks

--- pages/feedbacks.vue
+++ pages/feedbacks.vue
@@ -2,7 +2,6 @@
   <div>
     <h1>Feedbacks</h1>
     <section>
-      <h2>Feedback - Pin</h2>
       <section class="pined-card-section">
         <PinedCard
           title="SpaceCube Hardcore"
@@ -25,30 +24,47 @@
         />
       </section>
     </section>
+    <!-- TODO COMPONENTS   -->
     <section>
       <h2>Other Feedback</h2>
-      <ul>
-        <li>
-          blabla
-        </li>
-      </ul>
+      <div v-if='!pending'>
+        <ul
+          v-for="feedback in feedbacks"
+          :key='feedback.id'
+          >
+          <li>
+            {{ feedback.title }} created by {{ feedback.author.username }} at {{ feedback.createdAt }}
+          </li>
+        </ul>
+      </div>
+      <div v-if='pending'>
+        <Skeleton shape='list'/>
+        <Skeleton shape='list'/>
+        <Skeleton shape='list'/>
+        <Skeleton shape='list'/>
+      </div>
     </section>
 
   </div>
 </template>
 
-<script>
-export default {
-  name: 'FeedbacksPage',
-  layout: 'default',
-  head () {
-    return {
-      title: 'Space-Cube | Feedbacks'
-    };
+<script setup>
+import Skeleton from 'primevue/skeleton';
+
+useHead({
+  title: 'Space-Cube | Feedbacks',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
   }
-};
+});
+const { pending, data } = await useLazyAsyncData('feedbacks', () => $fetch('http://localhost:9080/feedbacks'));
+watch(data, (feedbacks) => {
+  // Because count starts out null, you won't have access
+  // to its contents immediately, but you can watch it.
+  console.log(feedbacks);
+  console.log(pending);
+});
 </script>
-
-<style scoped>
-
-</style>
