e4484c886bffa9207f1f8801f06aa426030f2a0e
Nick Notararigo <nicolas.notararigo@gmail.com>
2023-04-29 16:34:22 +0200

    fix: store missing in middleware

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,11 +1,10 @@
 const axios = require('axios').default;
-import { useAuthStore } from '../stores';
 
 console.log(axios)
-const token = await $fetch('/sign-in', {
-  method: 'post',
-  body: {
-    username: this.username,
-    password: this.password,
-  },
-});
+// const token = await $fetch('/sign-in', {
+//   method: 'post',
+//   body: {
+//     username: this.username,
+//     password: this.password,
+//   },
+// });
