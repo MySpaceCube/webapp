26e79723e1791e7fa75ca9ebc1f58d28c7f64d36
Nick Notararigo <nicolas.notararigo@gmail.com>
2023-07-10 14:06:21 +0200

    test cors

--- server/middleware/api-proxy.js
+++ server/middleware/api-proxy.js
@@ -6,13 +6,13 @@ module.exports = function (req, res, next) {
     target: process.env.NUXT_PUBLIC_API_URL,
     changeOrigin: true,
     pathRewrite: {
-      '^/api': '',
+      '^/api': ''
     },
     onProxyReq: (proxyReq) => {
       proxyReq.setHeader('Access-Control-Allow-Origin', process.env.NUXT_PUBLIC_API_URL);
       proxyReq.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, POST, PUT, PATCH, DELETE');
       proxyReq.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
-    },
+    }
   };
 
   const proxy = createProxyMiddleware(options);
